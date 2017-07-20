60ca3d8dde5c38d4c40d8e9e0dacf39e44ff7f1f
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-07-20 12:30:04 +0200

    hybrid recommender available via http call

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import os
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
+from recommender.hybrid import rec1, rec2, rec3
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
+
+@app.route('/recommend', methods=['GET'])
+def recommend():
+  weights = list(map(float, request.args.get('weights').split(',')))
+  context = list(map(int, request.args.get('context').split(',')))
+  if (len(context) != 8 or len(weights) != 3):
+    response = {"error":"context should consist exactly out of 8 features, weights should be exactly 3"}
+  else:
+    rm_1 = rec1.recommend(context)
+    rm_2 = rec2.recommend(context)
+    rm_3 = rec3.recommend(context)
+    rm_hybrid = rm_1*weights[0]+ rm_2*weights[1]+rm_3*weights[2]
+    response = {
+      "rec1":rm_1.tolist(),
+      "rec2":rm_2.tolist(),
+      "rec3":rm_3.tolist(),
+      "hybrid":rm_hybrid.tolist()
+    }
+  return jsonify(response)
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
---
+++ recommender/hybrid.py
@@ -0,0 +1,24 @@
+# Hybrid Recommender
+import json
+from recommender import Recommender
+
+with open('recommender/hybrid_config.json') as config_file:    
+    config = json.load(config_file)
+print (config)
+
+# RECOMMENDER 1
+rec1 = Recommender(config["rec1"]["default"])
+for rule in config["rec1"]["examples"]:
+    rec1.addRule(rule[0],rule[1])
+
+
+# RECOMMENDER 2
+rec2 = Recommender(config["rec2"]["default"])
+for rule in config["rec2"]["examples"]:
+    rec2.addRule(rule[0],rule[1])
+
+
+# RECOMMENDER 3
+rec3 = Recommender(config["rec3"]["default"])
+for rule in config["rec3"]["examples"]:
+    rec3.addRule(rule[0],rule[1])
