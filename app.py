e9357266d54204bf723622fd1101057d855d9baf
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-07-21 16:04:08 +0200

    code refactored (app.py cleaned up)

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
-from hybrid.hybrid import rec1, rec2, rec3
 
+from hybrid.helpers import getRecommendations
 
 app = Flask(__name__)
 CORS(app)
@@ -13,22 +13,7 @@ def index():
 
 @app.route('/recommend', methods=['GET'])
 def recommend():
-  weights = list(map(float, request.args.get('weights').split(',')))
-  context = list(map(int, request.args.get('context').split(',')))
-  if (len(context) != 8 or len(weights) != 3):
-    response = {"error":"context should consist exactly out of 8 features, weights should be exactly 3"}
-  else:
-    rm_1 = rec1.recommend(context)
-    rm_2 = rec2.recommend(context)
-    rm_3 = rec3.recommend(context)
-    rm_hybrid = rm_1*weights[0]+ rm_2*weights[1]+rm_3*weights[2]
-    response = {
-      "rec1":rm_1.tolist(),
-      "rec2":rm_2.tolist(),
-      "rec3":rm_3.tolist(),
-      "hybrid":rm_hybrid.tolist()
-    }
-  return jsonify(response)
+  return getRecommendations()
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
