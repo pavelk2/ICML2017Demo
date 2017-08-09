555881505206a0a9a99c35db2d8a9cb2b6d5657a
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-08-09 14:54:45 +0200

    removed bootstrap added nprogress to indicate loading

--- web/js/recommendations.js
+++ web/js/recommendations.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
     onFormChanged();
     $("form").change(onFormChanged);
+
 });
 
 onFormChanged = function() {
     var form_data = getFormData();
+    NProgress.configure({ showSpinner: false }).start(0.7);
     getRecommendations(form_data["context"], form_data["weights"], function(response) {
         console.log(response)
+        NProgress.done();
+        NProgress.remove();
         showRecommendations(response["recommenders"]);
         addSongs(response["songs"]);
     });
@@ -33,7 +37,6 @@ getRecommendations = function(context, weights, callback) {
     var context_string = context.join(","),
         weights_string = weights.join(","),
         base_url = "/recommend?";
-
     var url = base_url + "context=" + context_string + "&weights=" + weights_string;
     $.get(url, callback)
 }
@@ -49,22 +52,22 @@ addSongs = function(tracks) {
 }
 
 showRecommendations = function(recommenders) {
-    recommenders.forEach(function(recommender){
+    recommenders.forEach(function(recommender) {
         updateRecommender(recommender.name, recommender['recommendation'], recommender['is_reliable'])
     });
-   
+
 }
 updateRecommender = function(classname, recommendation, is_reliable) {
     $("." + classname + " .mood").text(recommendation[2])
     $("." + classname + " .tempo").text(Math.round(recommendation[1]))
     $("." + classname + " .genre").text(recommendation[0])
-    
+
     if (is_reliable == 1) {
         $("." + classname).removeClass("table-danger");
     } else {
         $("." + classname).addClass("table-danger");
-        $("." + classname + " .genre").html($("." + classname + " .genre").text()+'<span class="badge badge-danger">Recommender is not used as input is not reliable.</span>')
-        
+        $("." + classname + " .genre").html($("." + classname + " .genre").text() + '<span class="badge badge-danger">Recommender is not used as input is not reliable.</span>')
+
     }
 
 }
